71d361102c7097ef9b176b6f7737f4c6ce34ebf7
FedericoCortesi <cortesifederico6@gmail.com>
2025-01-14 21:01:06 +0100

    added studying today

--- Logger.py
+++ Logger.py
@@ -31,7 +31,7 @@ class Logger:
         # Set Studying to True 
         self.is_studying = True
 
-        print(f"Study session started at {self.start_time.strftime('%H:%M:%S')}")
+        print(f"\nStudy session started at {self.start_time.strftime('%H:%M:%S')}")
         
         # Call function to upadate timer 
         self._display_timer()
@@ -39,15 +39,28 @@ class Logger:
 
     def _display_timer(self):
         def update_timer():
+            # retrieve time passsed today
+            df_today, time_today = self._today_stats()
+
             while self.is_studying:
+                # Format the elapsed time
                 current_time = datetime.now()
                 elapsed = current_time - self.start_time
                 total_seconds = int(elapsed.total_seconds())
                 hours, remainder = divmod(total_seconds, 3600)
                 minutes, seconds = divmod(remainder, 60)
 
-                # Clear the line and print the elapsed time
-                print(f"\r{' ' * 30}\rStudying for: {hours:02}:{minutes:02}:{seconds:02}", end="")
+                
+                # Print total time today
+                time_studying = time_today + elapsed
+                total_seconds_today = int(time_studying.total_seconds())
+                hours_today, remainder_today = divmod(total_seconds_today, 3600)
+                minutes_today, seconds_today = divmod(remainder_today, 60)
+
+                # Print total time today
+                print(f"\r{' ' * 30}\rStudying for: {hours:02}:{minutes:02}:{seconds:02}\tTime spent studying today: {hours_today:02}:{minutes_today:02}:{seconds_today:02}", end="")
+
+                # Wait until next iteration
                 time.sleep(1)
 
         timer_thread = threading.Thread(target=update_timer, daemon=True)
@@ -107,5 +120,19 @@ class Logger:
         print(f"File saved at: ./Logs/study_sessions.csv")
 
 
+    def _open_log(self):
+        """Open the log file parsing the dates"""
+        df_log = pd.read_csv("./Logs/study_sessions.csv", index_col=0, parse_dates=['start_time', 'end_time'])
+        df_log['total_time'] = pd.to_timedelta(df_log['total_time'])
+        return df_log
 
 
+    def _today_stats(self):
+        """Get the stats for today"""
+        df_log = self._open_log()
+        today = datetime.now().date().strftime("%Y-%m-%d")
+        df_today = df_log[df_log['day'] == today]
+
+        time_today = df_today["total_time"].sum()
+        
+        return df_today, time_today
